6c082860a35ad20aa6712b173f1bf9340dd457b1
Jackson Miller <jacksonmiller7855@gmail.com>
2019-08-07 21:41:04 -0400

    created build task to copy .graphql files

---
+++ copyGraphQLFiles.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs";
+import recursiveReadDir = require("recursive-readdir");
+
+const copyFiles = () => {
+  recursiveReadDir("./src")
+    .then(files => files.filter(name => name.match(/\.*\.graphql/)))
+    .then(files =>
+      files.forEach(file => {
+        console.log(file);
+        const dest = file.replace(/^src/, "dist");
+        fs.mkdirSync(dest.replace(/\\{1}(\w*)\.graphql$/, ""), {
+          recursive: true
+        });
+        fs.copyFileSync(file, dest);
+      })
+    );
+};
+
+copyFiles();
+
+if (process.argv[2] === "--watch") {
+  fs.watch("./src", (eventType, fileName) => {
+    if (eventType === "change" && fileName.match(/.*\.graphql/)) {
+      copyFiles();
+    }
+  });
+}
