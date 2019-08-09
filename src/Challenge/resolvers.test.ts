6c64ccdf707330514f68950054d96648590b6297
Jackson Miller <jacksonmiller7855@gmail.com>
2019-08-08 22:35:26 -0400

    added type checking for tests

--- src/Challenge/resolvers.test.ts
+++ src/Challenge/resolvers.test.ts
@@ -1,13 +1,14 @@
 import ChallengeResolvers from "./resolvers";
+import { Challenge, User } from "../generated/graphql";
 
 test("transforms source data properly", () => {
-  const testUser = {
+  const testUser: User = {
     name: "Test",
     email: "test@example.com",
     displayName: "test"
   };
 
-  const testChallenge = {
+  const testChallenge: Challenge = {
     name: "test",
     startDate: "ewrg",
     endDate: "sdfg",
